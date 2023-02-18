ab4904e9fc217954e63c44e4966974f25bef342c
Rose Downes <g00311305@gmit.ie>
2023-02-18 12:32:15 +0000

    working on navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,26 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    
+
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Caheroyan House and Farm</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#link">Rooms</Nav.Link>
+              <NavDropdown title="More" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Gallery</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
-                  Another action
+                  Do and See
                 </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
-                  Separated link
+                  Contact
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
