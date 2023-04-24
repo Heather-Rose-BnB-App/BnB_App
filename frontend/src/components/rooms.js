4e93c81b032c94611bb587f5c1ff56437802ad3f
Rose Downes <g00311305@gmit.ie>
2023-04-24 17:09:48 +0100

    room page

--- frontend/src/components/rooms.js
+++ frontend/src/components/rooms.js
@@ -12,10 +12,41 @@ export class Rooms extends React.Component {
   
 
         return (
-            <div >
-                <BookingButton></BookingButton>
-                Hello
+                     <div className="room-container">
+                <h1>Rooms</h1>
+                    <BookingButton></BookingButton>
+                    
+                
+                <div className="room-gallery">
+                    <div className="room-card">
+                        <img src="/images/room1.jpg" alt="Room 1" />
+                        <h3>Birch Room</h3>
+                        <p>Our Double Room is a cozy retreat with a comfortable
+                            double bed, perfect for couples or solo travelers.</p>
+                    </div>
+                    <div className="room-card">
+                        <img src="/images/room2.jpg" alt="Room 2" />
+                        <h3>Ash Room</h3>
+                        <p>Our Twin Room features two comfortable single beds.</p>
+                    </div>
+                    <div className="room-card">
+                        <img src="/images/room3.jpg" alt="Room 3" />
+                        <h3>Oak Room</h3>
+                        <p>Our Double Room is a spacious and elegantly
+                            furnished room with a comfortable double bed.</p>
+                    </div>
+                    <div className="room-card">
+                        <img src="/images/room4.jpg" alt="Room 4" />
+                        <h3>Sycamore Room</h3>
+                        <p>Our Double Room is a charming room with a
+                            comfortable double bed and beautiful views of the
+                            surrounding countryside.</p>
+                    </div>
+                </div>
             </div>
+            
+            
+            
         );
     }
 }
